6f8b7106634783301220a5ff70ab5d6a77359302
limao996 <limao7325@163.com>
2025-05-07 15:50:49 +0800

    UPDATE 1.1.1-beta1 1. 使用md3组件库

--- composeApp/src/androidMain/kotlin/org/limao996/tomato_downloader/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/limao996/tomato_downloader/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.vinceglb.filekit.FileKit
@@ -26,15 +28,16 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (getIntent().getAction() != null && getIntent().getAction()
-                .equals(Intent.ACTION_SEND) && getIntent().getType() != null && "text/plain".equals(getIntent().getType())
+        if (intent.action != null && intent.action
+                .equals(Intent.ACTION_SEND) && intent.type != null && "text/plain".equals(intent.type)
         ) {
-            handleSendText(getIntent()); // 处理文本数据
+            handleSendText(intent); // 处理文本数据
         }
-
+        enableEdgeToEdge()
         activity = this
         FileKit.init(this)
         setContent {
